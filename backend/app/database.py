ad64dd3891f0a133f37c63323b19da8bc1d17fd1
Amadi israel <davidcarson380@gmail.com>
2026-08-09 13:27:46 +0100

    third commit

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,13 +1,13 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./store.db"
+load_dotenv()
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -18,8 +18,10 @@ SessionLocal = sessionmaker(
 Base = declarative_base()
 
 
+
 def get_db():
     db = SessionLocal()
+
     try:
         yield db
     finally:
